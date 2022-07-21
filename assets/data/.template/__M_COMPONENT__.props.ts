180951c728ad0b0f9935d1ca57f174d18390935c
minsoo kim <zlemzlem5656@naver.com>
2022-07-21 14:41:27 +0900

    fix: filename

---
+++ assets/data/.template/__M_COMPONENT__.props.ts
@@ -0,0 +1,5 @@
+interface __M_COMPONENT__ {
+  dummy?: any;
+}
+
+export default __M_COMPONENT__;
--- assets/data/.template/__M_COMPONENT__.tsx
+++ assets/data/.template/__M_COMPONENT__.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import * as Styled from "./__M_STYLE__.style";
+import __M_COMPONENT__Props from "./__M_COMPONENT__.props";
 
-function __M_COMPONENT__() {
+const __M_COMPONENT__ = () => {
   return <Styled.Container>hi</Styled.Container>;
-}
+};
 
 export default __M_COMPONENT__;
